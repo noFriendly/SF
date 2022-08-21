2fc5bd3a1a0ed1ece43982ffb5005e12f26e9d2e
Roman <mms280988@mail.ru>
2022-08-21 22:51:38 +0300

    commit

---
+++ NewsPaper/project/project/celery.py
@@ -0,0 +1,20 @@
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+app = Celery('project')
+app.config_from_object('django.conf:setting', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'action_every_monday_8am': {
+        'task': 'action',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+        'args': (agrs),
+    },
+}
